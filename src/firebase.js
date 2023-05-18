44b0fc704fc44b9f3c987d8a3d9202405d2658fb
Saranya <tsaranya03@gmail.com>
2023-05-18 22:01:24 +0530

    add

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,11 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBrsx-soOYhgey6exWomVa47htRyeMzw-I",
   authDomain: "connect-via-chat.firebaseapp.com",
@@ -9,9 +13,10 @@ const firebaseConfig = {
   storageBucket: "connect-via-chat.appspot.com",
   messagingSenderId: "846203429834",
   appId: "1:846203429834:web:3a4956264279c7fb426533",
-  measurementId: "G-CC25X6TEXD",
+  measurementId: "G-CC25X6TEXD"
 };
 // Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
